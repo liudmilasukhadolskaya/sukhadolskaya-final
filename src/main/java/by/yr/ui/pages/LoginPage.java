4750caa9aa6d10017fe73964c6e1cfab5a03e4f4
liudmilasukhadolskaya <liudmila.sukhadolskaya@gmail.com>
2025-09-19 12:49:32 +0200

    Refactor LoginPage, LoginTest (#3)
    * add @BeforeEach in BaseTest, refactor code
    
    * refactor code -LoginPage, LoginTest

--- src/main/java/by/yr/ui/pages/LoginPage.java
+++ src/main/java/by/yr/ui/pages/LoginPage.java
@@ -60,4 +60,9 @@ public class LoginPage {
     public String getPersonalAccountText() {
         return by.yr.ui.utils.WebDriver.getTextFromElement(TITLE_IN_PERSONAL_ACCOUNT);
     }
+
+
+    public void clickEmailField(){
+        by.yr.ui.utils.WebDriver.clickElement(INPUT_EMAIL);
+    }
 }
