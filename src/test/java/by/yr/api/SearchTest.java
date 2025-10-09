67da9f97221fb11ac513885d97ae420d78af6b67
liudmilasukhadolskaya <liudmila.sukhadolskaya@gmail.com>
2025-10-09 12:50:32 +0200

    refactor search api tests (#17)

--- src/test/java/by/yr/api/SearchTest.java
+++ src/test/java/by/yr/api/SearchTest.java
@@ -1,12 +1,14 @@
 package by.yr.api;
 
-import by.yr.utils.TestDataGenerator;
 import io.restassured.response.Response;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
 
 import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 public class SearchTest {
@@ -17,39 +19,41 @@ public class SearchTest {
         searchService = new SearchService();
     }
 
-    @Test
+    @ParameterizedTest
+    @CsvFileSource(resources = "/search_keywords.csv")
     @DisplayName("Verify search by popular keyword returns results")
-    public void searchByPopularKeyword() {
-        Response response = searchService.searchByPopularKeyword();
-        List<String>titles=searchService.getTitles(response);
+    public void searchByPopularKeyword(String keyword) {
+        Response response = searchService.sendSearchProductRequest(keyword);
+        List<String> titles = searchService.getTitles(response);
 
         assertAll("Response validation",
-                () -> assertEquals(200, searchService.getStatusCode(response), "Status code"),
-                () -> assertTrue(searchService.getDataSize(response)>0,  "data.size() > 0"),
-                () -> ApiAssertions.assertDefaultMeta(response));
+                () -> assertEquals(200, searchService.getStatusCode(response), "Status code mismatch"),
+                () -> assertTrue(searchService.getDataSize(response) > 0, "data.size() should be > 0")
+                );
     }
 
     @Test
     @DisplayName("Verify search for specific product")
     public void searchSpecificProduct() {
-        String keyword="Ondes";
+        String keyword = "парфюмерная вода ondes positives";
         Response response = searchService.sendSearchProductRequest(keyword);
-        List<String>titles=searchService.getTitles(response);
+        List<String> titles = searchService.getTitles(response);
+
         assertAll("Response validation",
-                () -> assertEquals(200,searchService.getStatusCode(response), "Status code"),
-                () -> assertTrue(searchService.getDataSize(response)>0,  "data.size() > 0"),
-                ()->assertTrue(titles.stream().anyMatch(title->title.contains(keyword)),"At least one item contains "+keyword),
-                () -> ApiAssertions.assertDefaultMeta(response));
+                () -> assertEquals(200, searchService.getStatusCode(response), "Status code mismatch"),
+                () -> assertTrue(searchService.getDataSize(response) > 0, "data.size() should be > 0"),
+                () -> assertTrue(searchService.containsKeyword(titles, keyword), "Expected at least one product title to contain " + keyword)
+               );
     }
 
     @Test
     @DisplayName("Verify the search when no results found")
     public void searchWithNoResults() {
-        String keyword="keyboard";
+        String keyword = "keyboard";
         Response response = searchService.sendSearchProductRequest(keyword);
         assertAll("Response validation",
-                () -> assertEquals(200, searchService.getStatusCode(response), "Status code"),
-                () -> assertEquals(0, searchService.getDataSize(response), "Data size"),
-                () -> ApiAssertions.assertDefaultMeta(response));
+                () -> assertEquals(200, searchService.getStatusCode(response), "Status code mismatch"),
+                () -> assertEquals(0, searchService.getDataSize(response), "Ddata.size() should be 0")
+               );
     }
 }
