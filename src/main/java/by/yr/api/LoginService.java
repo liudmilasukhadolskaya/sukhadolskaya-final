e48a71768e99a558146bf9403f6fe8d3ded48832
liudmilasukhadolskaya <liudmila.sukhadolskaya@gmail.com>
2025-10-09 13:16:05 +0200

    refactor tests, add @Step (#18)

--- src/main/java/by/yr/api/LoginService.java
+++ src/main/java/by/yr/api/LoginService.java
@@ -1,11 +1,13 @@
 package by.yr.api;
 
 import com.google.gson.JsonObject;
+import io.qameta.allure.Step;
 import io.restassured.response.Response;
 
 public class LoginService {
     private Response lastResponse;
 
+    @Step(" Send Login Request and Get Response")
     public Response sendLoginRequest(String email, String password, boolean remember) {
         ApiService apiService = new ApiService();
         String body = buildLoginBody(email, password, remember);
@@ -21,22 +23,27 @@ public class LoginService {
         return json.toString();
     }
 
+    @Step("Get errors.email from Response")
     public String getEmailError() {
         return lastResponse.path("errors.email[0]");
     }
 
+    @Step("Get errors.password from Response")
     public String getPasswordError() {
         return lastResponse.path("errors.password[0]");
     }
 
+    @Step("Get message from Response")
     public String getMessage() {
         return lastResponse.path("message");
     }
 
+    @Step("Get Status Code from Response")
     public int getStatusCode() {
         return lastResponse.getStatusCode();
     }
 
+    @Step("Get Token from Response")
     public String getToken() {
         return lastResponse.path("token");
     }
